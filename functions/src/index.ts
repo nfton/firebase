d8daca1ecd2ac49029162e86bd22c870d23b0f8f
Jakhongir <tashpulatov.jakhongir@gmail.com>
2022-09-29 14:35:26 +0200

    created react app

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,26 @@
 import * as functions from "firebase-functions";
+import {Telegraf} from 'telegraf'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const bot = new Telegraf(functions.config().telegram.token, {
+	telegram: {webhookReply: true},
+})
+
+bot.start(ctx => {
+	ctx.reply("Launch Game by clicking this button:", {
+		reply_markup: {
+			inline_keyboard: [[{
+				web_app: {url: 'https://start.nfton.space'},
+				text: "Launch Game"
+			}]]
+		}
+	})
+})
+
+export const telegramBot = functions
+	.region('europe-west3')
+	.https
+	.onRequest(async (req, res) => {
+		await bot.handleUpdate(req.body, res).then((rv: any) => {
+			return !rv && res.status(200).send('ok')
+		})
+	})
